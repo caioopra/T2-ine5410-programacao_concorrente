f5128b3bc2c470903ad334d596bceb66ff302832
fillipimangrich <costa.mangrich@gmail.com>
2022-12-12 14:47:42 -0300

    fix log queue

--- payment_system/payment_processor.py
+++ payment_system/payment_processor.py
@@ -54,6 +54,7 @@ class PaymentProcessor(Thread):
                 self.bank.queue_semaphore.acquire()
                 transaction = queue.pop(0)
                 LOGGER.info(f"Transaction_queue do Banco {self.bank._id}, tamanho da fila :{len(queue)}")
+                # LOGGER.info(f"Transaction_queue do Banco {self.bank._id}: {queue}")
                 # LOGGER.info(f"{self.bank._id} ")  # TODO: trocar de volta o print
             except Exception as err:
                 LOGGER.error(f"Falha em PaymentProcessor.run(): {err}")
